84e8241092160c95b5eb9e6be95b378cf9b6715d
Gustavo Preste <gustavo.preste@escola.pr.gov.br>
2026-05-29 11:11:25 +0000

    aula3/2

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
+const abas = document.querySelectorAll(".aba-conteudo");
 
 botoes.forEach((botao) => {
   botao.addEventListener("click", () => {
+    const indice = Number(botao.dataset.index);
+
     botoes.forEach((item) => {
       item.classList.remove("ativo");
       item.setAttribute("aria-pressed", "false");
     });
 
+    abas.forEach((aba) => {
+      aba.classList.remove("ativo");
+    });
+
     botao.classList.add("ativo");
     botao.setAttribute("aria-pressed", "true");
+    abas[indice]?.classList.add("ativo");
   });
 });
